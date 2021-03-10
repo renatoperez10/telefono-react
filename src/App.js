91007f1e4582b780368d704442cfda8f74d6470e
Miquel Castro <castro-miquel@hotmail.com>
2021-03-10 19:06:19 +0100

    Hago los botones de manera mas pro con un for y un map. Les doy sus funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
+import {  useState } from "react"
+
 function App() {
+  const [numTelefono, setNumTelefono] = useState("");
+
+  const numerosBotones = () => {
+    let botones = [];
+    for(let i = 1; i < 10; i++){
+      botones.push(i)
+    }
+    return botones
+  }
+
+  const marcarNumero = (e) => {
+    setNumTelefono(numTelefono + e.target.innerHTML)
+  }
+  const borrarNumero = () => {
+    setNumTelefono(numTelefono.slice(0, -1))
+  }
+
+
   return (
     <div class="contenedor">
-      <!-- El siguiente elemento se oculta añadiéndole la clase "off" -->
       <span class="mensaje">Llamando...</span>
       <main class="telefono">
         <div class="botones">
           <ol class="teclado">
-            <li><button>1</button></li>
-            <li><button>2</button></li>
-            <li><button>3</button></li>
-            <li><button>4</button></li>
-            <li><button>5</button></li>
-            <li><button>6</button></li>
-            <li><button>7</button></li>
-            <li><button>8</button></li>
-            <li><button>9</button></li>
-            <li><button>0</button></li>
-            <li><button class="big">borrar</button></li>
+            {numerosBotones().map(nBoton => <li key={nBoton}><button onClick={marcarNumero} >{nBoton}</button></li>)}
+            <li><button onClick={marcarNumero}>0</button></li>
+            <li><button onClick={borrarNumero} class="big">borrar</button></li>
           </ol>
         </div>
         <div class="acciones">
-          <span class="numero">667359961</span>
-          <!-- El botón de llamar debe tener la clase "activo" cuando -->
-                <!-- el número de teléfono tiene 9 cifras -->
-                <a href="#" class="llamar">Llamar</a>
-          <!-- Sólo se tiene que ver un botón u otro -->
-                <a href="#" class="colgar activo">Colgar</a>
+          <span class="numero">{numTelefono}</span>
+                <a href="llamar" class="llamar">Llamar</a>
+                <a href="colgar" class="colgar activo">Colgar</a>
         </div>
       </main>
     </div>
